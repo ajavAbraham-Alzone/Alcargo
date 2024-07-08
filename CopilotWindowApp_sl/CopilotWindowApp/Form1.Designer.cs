5b7319448a67754c18ac14c030a6224aac15dff4
Ajay Abraham-Alzone <159514807+ajavAbraham-Alzone@users.noreply.github.com>
2024-07-08 11:17:59 +0530

    sample application version 1.0

---
+++ CopilotWindowApp_sl/CopilotWindowApp/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace CopilotWindowApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ClickHere = new Button();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // ClickHere
+            // 
+            ClickHere.Location = new Point(256, 192);
+            ClickHere.Name = "ClickHere";
+            ClickHere.Size = new Size(75, 23);
+            ClickHere.TabIndex = 1;
+            ClickHere.Text = "Click Here";
+            ClickHere.UseVisualStyleBackColor = true;
+            ClickHere.Click += ClickHere_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(106, 104);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Type Here";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(242, 104);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(267, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(ClickHere);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button ClickHere;
+        private Label label1;
+        private TextBox textBox2;
+    }
+}
---
+++ CopilotWindowApp_sl/CopilotWindowApp/Form1.cs
@@ -0,0 +1,17 @@
+namespace CopilotWindowApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickHere_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.typeHereString = textBox2.Text;
+            form2.Show();
+        }
+    }
+}
